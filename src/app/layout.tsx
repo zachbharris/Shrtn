2557153f7457804b42257bce06d590851725ec5a
zachbharris <hi@zachbharris.com>
2023-05-19 12:42:54 -0500

    meta data changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,8 @@
+import Head from "next/head";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Shrtn URLs",
-  description: "Quickly generate short URLs for sharing.",
-};
 
 export default function RootLayout({
   children,
@@ -18,8 +14,15 @@ export default function RootLayout({
       lang="en"
       className="bg-zinc-50 dark:bg-zinc-950 text-zinc-800 dark:text-zinc-300 h-full w-full selection:bg-blue-800"
     >
+      <Head>
+        <title>Shrtn</title>
+        <meta name="description" content="Simple Short URL Generator." />
+        <meta property="og:image" content="/public/og-image.png" />
+        <meta property="og:title" content="/public/og-image.png" />
+        <meta property="og:description" content="/public/og-image.png" />
+      </Head>
       <body
-        className={`${inter.className} h-full w-full flex items-start md:items-center justify-center overflow-hidden`}
+        className={`${inter.className} h-full w-full flex items-start md:items-center justify-center`}
       >
         {children}
       </body>
