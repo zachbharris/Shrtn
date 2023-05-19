9f48cefcab81a4709f4fcc556a8b103a61849315
zachbharris <hi@zachbharris.com>
2023-05-19 12:44:58 -0500

    move head back to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
-import Head from "next/head";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Shrtn",
+  description: "Simple Short URL Generator.",
+  "og:image": "/public/og-image.png",
+};
 
 export default function RootLayout({
   children,
@@ -14,13 +19,6 @@ export default function RootLayout({
       lang="en"
       className="bg-zinc-50 dark:bg-zinc-950 text-zinc-800 dark:text-zinc-300 h-full w-full selection:bg-blue-800"
     >
-      <Head>
-        <title>Shrtn</title>
-        <meta name="description" content="Simple Short URL Generator." />
-        <meta property="og:image" content="/public/og-image.png" />
-        <meta property="og:title" content="/public/og-image.png" />
-        <meta property="og:description" content="/public/og-image.png" />
-      </Head>
       <body
         className={`${inter.className} h-full w-full flex items-start md:items-center justify-center`}
       >
