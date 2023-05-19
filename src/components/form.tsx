129ba51331135a55cee492debdff88c0b5d39849
zachbharris <hi@zachbharris.com>
2023-05-19 01:32:15 -0500

    new favicon, animations and more

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,32 +1,71 @@
 "use client";
 
-import { useState } from "react";
-import Icon from "./icon";
+import { useEffect, useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { AnimatePresence, motion } from "framer-motion";
 
-type Status = "idle" | "loading" | "success" | "error";
+import Icon from "./icon";
+import { z } from "zod";
+
+type Inputs = {
+  url: string;
+};
+
+type Status = "idle" | "loading" | "success" | "error" | "copied";
+
+const formSchema = z.object({
+  url: z.string().url().nonempty("URL can't be empty"),
+});
 
 export default function Form() {
-  const [url, setUrl] = useState("");
-  const [pathUrl, setPathUrl] = useState("");
+  const [pathUrl, setPathUrl] = useState(
+    "https://thisisareallylongurlthatkeepsgoingonforeverandireally.com/shouldfixthatyouknow"
+  );
   const [status, setStatus] = useState<Status>("idle");
   const [redirect, setRedirect] = useState();
 
-  async function handleSubmit(e: any) {
-    e.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>({ resolver: zodResolver(formSchema) });
 
-    setStatus("loading");
+  const onSubmit: SubmitHandler<Inputs> = async (e: any) => {
+    switch (status) {
+      case "success":
+        handleCopy();
+        setStatus("copied");
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        setStatus("idle");
+        reset();
+        break;
+      case "idle": {
+        setStatus("loading");
+        const values = getValues();
+        const res = await fetch("/api/urls", {
+          method: "POST",
+          body: JSON.stringify({ url: values.url }),
+        });
 
-    const res = await fetch("/api/urls", {
-      method: "POST",
-      body: JSON.stringify({ url }),
-    });
+        const { data } = await res.json();
 
-    const { data } = await res.json();
+        setRedirect(data.id);
+        setPathUrl(`${window.location.href}${data.id}`);
+        setStatus("success");
+        setStatus("success");
+        break;
+      }
+    }
+  };
 
-    setRedirect(data.id);
-    setPathUrl(`${window.location.href}${data.id}`);
-    setUrl("");
+  function handleRestart() {
+    reset();
     setStatus("idle");
+    setRedirect(undefined);
+    setPathUrl("");
   }
 
   function handleCopy() {
@@ -34,47 +73,99 @@ export default function Form() {
   }
 
   return (
-    <div className="">
-      <label htmlFor="new-url" className="flex flex-col gap-2 mb-8">
-        <span>Destination URL</span>
-        <input
-          type="url"
-          name="url"
-          id="new-url"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-          placeholder="https://example.com"
-          className="rounded-md px-4 py-2 bg-zinc-100/70 dark:bg-zinc-900/70"
-        />
-      </label>
-      <div className="relative block w-full">
-        {status === "loading" ? (
-          <Icon
-            name="loading"
-            className="absolute inset-0 m-auto animate-spin"
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="url" className="flex flex-col gap-2 mb-8">
+          <span>Destination URL</span>
+          <input
+            {...register("url", {
+              required: true,
+            })}
+            type="url"
+            autoFocus
+            placeholder="https://example.com"
+            className="rounded-md px-4 py-2 bg-zinc-100/70 dark:bg-zinc-900/70 focus:outline-none focus :ring focus:ring-blue-500"
           />
-        ) : null}
+          {errors.url ? (
+            <span className="text-red-500">
+              {errors.url.message ?? "Something went wrong."}
+            </span>
+          ) : null}
+        </label>
 
-        <button
-          disabled={status === "loading"}
-          type="submit"
-          onClick={handleSubmit}
-          className={`flex flex-row w-full justify-center items-center gap-2 text-zinc-50 bg-blue-500 enabled:hover:bg-blue-600 dark:bg-blue-700 dark:enabled:hover:bg-blue-800 p-2 rounded-md fill-blue-400 dark:fill-blue-700 mb-8 ${
-            status === "loading" ? "opacity-25" : ""
-          }`}
-        >
-          Submit
-        </button>
-      </div>
+        <div className="relative flex flex-row gap-4">
+          <motion.button
+            disabled={status === "loading"}
+            type="submit"
+            className={`relative flex flex-row w-full justify-center items-center text-zinc-50 bg-blue-500 enabled:hover:bg-blue-600 dark:bg-blue-700 dark:enabled:hover:bg-blue-800 px-4 py-2 rounded-md fill-blue-400 dark:fill-blue-700 transition-colors ease-in-out overflow-hidden h-10`}
+            initial={{ width: "100%" }}
+            animate={{ width: "100%" }}
+          >
+            <AnimatePresence initial={false}>
+              {status === "loading" ? (
+                <motion.div
+                  key="loading"
+                  initial={{ y: -40, position: "absolute" }}
+                  animate={{ y: 0, position: "relative" }}
+                  exit={{ y: 40, position: "absolute" }}
+                >
+                  <Icon name="loading" className="animate-spin" />
+                </motion.div>
+              ) : status === "success" ? (
+                <motion.div
+                  key="success"
+                  initial={{ y: -40, position: "absolute" }}
+                  animate={{ y: 0, position: "relative" }}
+                  exit={{ y: 40, position: "absolute" }}
+                  className="grid grid-flow-col justify-between gap-2 items-center"
+                >
+                  <span className="text-ellipsis overflow-hidden ">
+                    {pathUrl}
+                  </span>
+                  <Icon name="copy" />
+                </motion.div>
+              ) : status === "copied" ? (
+                <motion.div
+                  key="copied"
+                  initial={{ y: -40, position: "absolute" }}
+                  animate={{ y: 0, position: "relative" }}
+                  exit={{ y: 40, position: "absolute" }}
+                  className="flex flex-row gap-2 items-center"
+                >
+                  <Icon name="check" />
+                  <span>Copied</span>
+                </motion.div>
+              ) : (
+                <motion.div
+                  key="idle"
+                  initial={{ y: -40, position: "absolute" }}
+                  animate={{ y: 0 }}
+                  exit={{ y: 40, position: "absolute" }}
+                  className="flex flex-row gap-2 justify-center items-center"
+                >
+                  <Icon name="magic" />
+                  <span>shrtn</span>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </motion.button>
 
-      {status !== "loading" && redirect ? (
-        <div className="flex flex-row w-full justify-between items-center p-4 bg-zinc-200/70 dark:bg-zinc-900/70 rounded-md">
-          <span>{pathUrl}</span>
-          <button onClick={handleCopy}>
-            <Icon name="copy" />
-          </button>
+          <AnimatePresence>
+            {!["idle", "loading"].includes(status) ? (
+              <motion.button
+                key="restart"
+                initial={{ x: 100, opacity: 0, position: "absolute" }}
+                animate={{ x: 0, opacity: 1, position: "relative" }}
+                type="button"
+                onClick={handleRestart}
+                className="inline-flex flex-row justify-center items-center bg-transparent hover:bg-zinc-900/10 hover:dark:bg-zinc-100/10 h-10 rounded-md px-4 transition-colors ease-in-out"
+              >
+                <Icon name="refresh" />
+              </motion.button>
+            ) : null}
+          </AnimatePresence>
         </div>
-      ) : null}
+      </form>
     </div>
   );
 }
--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,6 +1,6 @@
 import { SVGProps, createElement } from "react";
 
-type IconName = "copy" | "loading";
+type IconName = "copy" | "loading" | "magic" | "check" | "refresh";
 
 type Props = {
   className?: string;
@@ -19,12 +19,20 @@ export default function Icon({ name, className }: Props) {
       return createElement(CopyIcon, props);
     case "loading":
       return createElement(LoadingIcon, props);
+    case "magic":
+      return createElement(MagicIcon, props);
+    case "check":
+      return createElement(CheckIcon, props);
+    case "refresh":
+      return createElement(RefreshIcon, props);
     default:
       return null;
   }
 }
 
-const CopyIcon = (props: SVGProps<SVGSVGElement>) => {
+type IconProps = SVGProps<SVGSVGElement>;
+
+const CopyIcon = (props: IconProps) => {
   return (
     <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
       <path d="M272 0h124.1C408.8 0 421 5.1 430 14.1L497.9 82c9 9 14.1 21.2 14.1 33.9V336c0 26.5-21.5 48-48 48H272c-26.5 0-48-21.5-48-48V48c0-26.5 21.5-48 48-48zM48 128h144v64H64v256h192v-32h64v48c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V176c0-26.5 21.5-48 48-48z"></path>
@@ -32,10 +40,34 @@ const CopyIcon = (props: SVGProps<SVGSVGElement>) => {
   );
 };
 
-const LoadingIcon = (props: SVGProps<SVGSVGElement>) => {
+const LoadingIcon = (props: IconProps) => {
   return (
     <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
       <path d="M477.7 384c21.8-37.7 34.3-81.4 34.3-128C512 114.6 397.4 0 256 0v64c106 0 192 86 192 192 0 35-9.4 67.8-25.7 96l55.4 32z"></path>
     </svg>
   );
 };
+
+const MagicIcon = (props: IconProps) => {
+  return (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+      <path d="M234.7 42.7L197 56.8c-3 1.1-5 4-5 7.2s2 6.1 5 7.2l37.7 14.1 14.1 37.7c1.1 3 4 5 7.2 5s6.1-2 7.2-5l14.1-37.7L315 71.2c3-1.1 5-4 5-7.2s-2-6.1-5-7.2l-37.7-14.1L263.2 5c-1.1-3-4-5-7.2-5s-6.1 2-7.2 5l-14.1 37.7zM46.1 395.4c-18.7 18.7-18.7 49.1 0 67.9l34.6 34.6c18.7 18.7 49.1 18.7 67.9 0l381.3-381.4c18.7-18.7 18.7-49.1 0-67.9l-34.6-34.5c-18.7-18.7-49.1-18.7-67.9 0L46.1 395.4zM484.6 82.6l-105 105-23.3-23.3 105-105 23.3 23.3zM7.5 117.2C3 118.9 0 123.2 0 128s3 9.1 7.5 10.8L64 160l21.2 56.5c1.7 4.5 6 7.5 10.8 7.5s9.1-3 10.8-7.5L128 160l56.5-21.2c4.5-1.7 7.5-6 7.5-10.8s-3-9.1-7.5-10.8L128 96l-21.2-56.5c-1.7-4.5-6-7.5-10.8-7.5s-9.1 3-10.8 7.5L64 96 7.5 117.2zm352 256c-4.5 1.7-7.5 6-7.5 10.8s3 9.1 7.5 10.8L416 416l21.2 56.5c1.7 4.5 6 7.5 10.8 7.5s9.1-3 10.8-7.5L480 416l56.5-21.2c4.5-1.7 7.5-6 7.5-10.8s-3-9.1-7.5-10.8L480 352l-21.2-56.5c-1.7-4.5-6-7.5-10.8-7.5s-9.1 3-10.8 7.5L416 352l-56.5 21.2z"></path>
+    </svg>
+  );
+};
+
+function CheckIcon(props: IconProps) {
+  return (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+      <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7l233.4-233.3c12.5-12.5 32.8-12.5 45.3 0z"></path>
+    </svg>
+  );
+}
+
+function RefreshIcon(props: IconProps) {
+  return (
+    <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <path d="M142.9 142.9c62.2-62.2 162.7-62.5 225.3-1L327 183c-6.9 6.9-8.9 17.2-5.2 26.2S334.3 224 344 224h128c13.3 0 24-10.7 24-24V72c0-9.7-5.8-18.5-14.8-22.2S461.9 48.1 455 55l-41.6 41.6c-87.6-86.5-228.7-86.2-315.8 1-24.4 24.4-42 53.1-52.8 83.8-5.9 16.7 2.9 34.9 19.5 40.8s34.9-2.9 40.8-19.5c7.7-21.8 20.2-42.3 37.8-59.8zM16 312v128c0 9.7 5.8 18.5 14.8 22.2s19.3 1.7 26.2-5.2l41.6-41.6c87.6 86.5 228.7 86.2 315.8-1 24.4-24.4 42.1-53.1 52.9-83.7 5.9-16.7-2.9-34.9-19.5-40.8s-34.9 2.9-40.8 19.5c-7.7 21.8-20.2 42.3-37.8 59.8-62.2 62.2-162.7 62.5-225.3 1L185 329c6.9-6.9 8.9-17.2 5.2-26.2S177.7 288 168 288H40c-13.3 0-24 10.7-24 24z"></path>
+    </svg>
+  );
+}
